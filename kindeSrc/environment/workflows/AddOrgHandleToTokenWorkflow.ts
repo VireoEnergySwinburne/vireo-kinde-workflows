cb4195a73631b9d629f164dcc5503c0e6558cd1e
Jed Saxon <jed-sax@proton.me>
2025-11-03 21:32:33 +1100

    cleanup code and use proper naming conventions for jwt

--- kindeSrc/environment/workflows/AddOrgHandleToTokenWorkflow.ts
+++ kindeSrc/environment/workflows/AddOrgHandleToTokenWorkflow.ts
@@ -9,6 +9,9 @@ export const workflowSettings: WorkflowSettings = {
   id: "add_org_handle",
   name: "Add Organization Handle to Access Token",
   trigger: WorkflowTrigger.UserTokenGeneration,
+  failurePolicy: {
+    action: "stop",
+  },
   bindings: {
     "kinde.accessToken": {},
     "kinde.mfa": {},
@@ -19,8 +22,10 @@ export default async function (event: onUserTokenGeneratedEvent) {
   // const userId = event.context.user.id;
 
   const orgHandle = "vireo-energy";
-  console.warn(`currently using hard-coded org handle '${orgHandle}) for debug purposes`);
+  console.warn(
+    `currently using hard-coded org handle '${orgHandle}' for debug purposes`,
+  );
 
-  const claims = accessTokenCustomClaims<{ orgHandle?: string }>();
-  claims.orgHandle = orgHandle;
+  const claims = accessTokenCustomClaims<{ org_handle?: string }>();
+  claims.org_handle = orgHandle;
 }
